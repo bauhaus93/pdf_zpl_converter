7bc263bdf6be68ff421b6f82cfde80363bd2a055
Jakob Fischer <schlemihl@lembas.me>
2025-12-24 11:27:58 +0100

    Adds nginx example, limit memory

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 mkdir -p /tmp/converter
 sudo chown www-data:www-data -R /tmp/converter
-docker run -d  -v /tmp/converter:/tmp --name converter pdf_converter
+docker run -d \
+	-v /tmp/converter:/tmp \
+	-m 256M \
+	--name converter \
+	pdf_converter
